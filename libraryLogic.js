792b856712c582a9ebcfb3a6c87ca13de011489f
Ivan <fivanunam@gmail.com>
2020-04-15 12:32:37 -0500

    add logic to store our books

--- libraryLogic.js
+++ libraryLogic.js
@@ -1,18 +1,18 @@
-// let myLibrary = [];
-//
-// function Book(author, title, nP) {
-//   this.author = author;
-//   this.title = title;
-//   this.numberPages = nP;
-// }
-//
-// function addBookToLibrary(author, title, nP) {
-//   let newB = new Book(author, title, nP);
-//   myLibrary.push();
-// }
-//
-//
-// console.log(myLibrary);
+let myLibrary = [];
+
+function Book(title, nP, author, read) {
+  this.author = author;
+  this.title = title;
+  this.numberPages = nP;
+  this.read = read;
+}
+
+function addBookToLibrary(book) {
+  myLibrary.push(book);
+}
+
+
+console.log(myLibrary);
 
 
 
@@ -20,13 +20,17 @@
 const button = document.getElementById('button');
 button.addEventListener('click', event => {
   const form = document.getElementById('form');
-  if (validateTitle(form.titleForm.value) && validatePages(form.numberPages.value)) {
-    const input = {
-      title : form.titleForm.value,
-      number : form.numberPages.value
-    }
-    console.log(input);
+  if (validateTitle(form.titleForm.value) && validatePages(form.numberForm.value)) {
+    const book = new Book(
+      form.titleForm.value,
+      form.numberForm.value,
+      form.authorForm.value,
+      form.readForm.checked)
+    
+    addBookToLibrary(book);
   }
+
+  console.log(myLibrary);
 });
 
 function validateTitle(title)
@@ -36,6 +40,16 @@ function validateTitle(title)
     return (true)
   }
   alert("You have entered an invalid title, must be bigger than 5 chars")
+  return (false)
+}
+
+function validateAuthor(author)
+{
+  if (author.length > 3)
+  {
+    return (true)
+  }
+  alert("You have entered an invalid author, must be bigger than 3 chars")
   return (false)
 }
 
