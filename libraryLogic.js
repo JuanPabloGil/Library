a8eb295f0fe75d88818bfab4d120b29cf380175b
Juan Pablo <jpablomgil@gmail.com>
2020-04-15 16:26:26 -0500

    Fixing problems of eslint

--- libraryLogic.js
+++ libraryLogic.js
@@ -1,5 +1,5 @@
 // storage
-let myLibrary = retrieveArr();
+const myLibrary = retrieveArr();
 render(myLibrary);
 
 // constructor
@@ -17,16 +17,16 @@ function addBookToLibrary(book) {
   render(myLibrary);
 }
 
-function deleteBook(index){
+function deleteBook(index) {
   myLibrary.splice(index, 1);
   saveArr(myLibrary);
   render(myLibrary);
 }
 
-function readedBook(index){
-  if ( myLibrary[index].read == true ){
+function readedBook(index) {
+  if (myLibrary[index].read == true) {
     myLibrary[index].read = false;
-  }else {
+  } else {
     myLibrary[index].read = true;
   }
   saveArr(myLibrary);
@@ -34,37 +34,35 @@ function readedBook(index){
 }
 
 // show methods
-function render(books){
-  let htmlBooks = "";
-  books.forEach((book,index) => {
-
-    htmlBooks +=  `
+function render(books) {
+  let htmlBooks = '';
+  books.forEach((book, index) => {
+    htmlBooks += `
       <div class="card">
         <div class="card-body">
           <h5 class="card-title">${book.title}</h5>
           <h6 class="card-subtitle mb-2 text-muted">Author: ${book.author}</h6>
           <p class="card-text">Number of Pages ${book.numberPages}</p>
 
-            <button class="btn btn-info" onclick="readedBook(${index})"> Status: ${book.read ? "Readed": "Not reded" } </button>
+            <button class="btn btn-info" onclick="readedBook(${index})"> Status: ${book.read ? 'Readed' : 'Not reded'} </button>
 
         </div>
           <button class="btn btn-info" onclick="deleteBook(${index})">Delete book</button>
       </div>
-      ` ;
+      `;
   });
 
   const containerBooks = document.querySelector('.container-books');
   containerBooks.innerHTML = htmlBooks;
-
 }
 
-function showForm(){
-  form = document.getElementById("hideShow");
-  if (form.style.display === "none") {
-      form.style.display = "block";
-    } else {
-      form.style.display = "none";
-    }
+function showForm() {
+  form = document.getElementById('hideShow');
+  if (form.style.display === 'none') {
+    form.style.display = 'block';
+  } else {
+    form.style.display = 'none';
+  }
 }
 
 // localStorage methods
@@ -72,10 +70,9 @@ function saveArr(arr) {
   window.localStorage.setItem('library', JSON.stringify(arr));
 }
 
-function retrieveArr(){
+function retrieveArr() {
   let arr = [];
-  if (window.localStorage.getItem("library") !== null)
-    arr = JSON.parse(window.localStorage.getItem('library'));
+  if (window.localStorage.getItem('library') !== null) arr = JSON.parse(window.localStorage.getItem('library'));
   return arr;
 }
 
@@ -88,44 +85,35 @@ button.addEventListener('click', event => {
       form.titleForm.value,
       form.numberForm.value,
       form.authorForm.value,
-      form.readForm.checked)
+      form.readForm.checked,
+    );
 
-      addBookToLibrary(book);
-    }
-  });
-
-
-  // validate methods
-  function validateTitle(title)
-  {
-    if (title.length > 5)
-    {
-      return (true)
-    }
-    alert("You have entered an invalid title, must be bigger than 5 chars")
-    return (false)
+    addBookToLibrary(book);
   }
+});
 
-  function validateAuthor(author)
-  {
-    if (author.length > 3)
-    {
-      return (true)
-    }
-    alert("You have entered an invalid author, must be bigger than 3 chars")
-    return (false)
+
+// validate methods
+function validateTitle(title) {
+  if (title.length > 5) {
+    return (true);
   }
+  alert('You have entered an invalid title, must be bigger than 5 chars');
+  return (false);
+}
 
-  function validatePages(number)
-  {
-
-    if(number > 30)
-    {
-      return true;
-    }
-    else
-    {
-      alert(" The book must have more than 30 pages ");
-      return false;
-    }
+function validateAuthor(author) {
+  if (author.length > 3) {
+    return (true);
   }
+  alert('You have entered an invalid author, must be bigger than 3 chars');
+  return (false);
+}
+
+function validatePages(number) {
+  if (number > 30) {
+    return true;
+  }
+  alert(' The book must have more than 30 pages ');
+  return false;
+}
