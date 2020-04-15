1201f24e8a102066b71e31047afe5dd80f67c719
Juan Pablo <jpablomgil@gmail.com>
2020-04-15 14:27:13 -0500

    Display books (Not working, diplays repeted books)

--- libraryLogic.js
+++ libraryLogic.js
@@ -9,8 +9,30 @@ function Book(title, nP, author, read) {
 
 function addBookToLibrary(book) {
   myLibrary.push(book);
+  render();
 }
 
+function render(){
+
+  const title = 'My book';
+
+  let text = ""
+    for (let i = 0 ; i < myLibrary.length; i += 1){
+     text +=  `
+      <div class="card">
+        <div class="card-body">
+          <h5 class="card-title">${myLibrary[i].title}</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Author: ${myLibrary[i].author}</h6>
+          <p class="card-text">Number of Pages ${myLibrary[i].numberPages}</p>
+        </div>
+      </div>
+      ` ;
+    }
+
+  const container = document.querySelector('.container');
+  container.insertAdjacentHTML('beforeend', text);
+
+}
 
 console.log(myLibrary);
 
@@ -26,58 +48,43 @@ button.addEventListener('click', event => {
       form.numberForm.value,
       form.authorForm.value,
       form.readForm.checked)
-    
-    addBookToLibrary(book);
-  }
 
-  console.log(myLibrary);
-});
+      addBookToLibrary(book);
+    }
 
-function validateTitle(title)
-{
-  if (title.length > 5)
+    console.log(myLibrary);
+  });
+
+  function validateTitle(title)
   {
-    return (true)
+    if (title.length > 5)
+    {
+      return (true)
+    }
+    alert("You have entered an invalid title, must be bigger than 5 chars")
+    return (false)
   }
-  alert("You have entered an invalid title, must be bigger than 5 chars")
-  return (false)
-}
 
-function validateAuthor(author)
-{
-  if (author.length > 3)
+  function validateAuthor(author)
   {
-    return (true)
+    if (author.length > 3)
+    {
+      return (true)
+    }
+    alert("You have entered an invalid author, must be bigger than 3 chars")
+    return (false)
   }
-  alert("You have entered an invalid author, must be bigger than 3 chars")
-  return (false)
-}
 
-
-function validatePages(number)
-{
-
-  if(number > 30)
+  function validatePages(number)
   {
-    return true;
+
+    if(number > 30)
+    {
+      return true;
+    }
+    else
+    {
+      alert(" The book must have more than 30 pages ");
+      return false;
+    }
   }
-  else
-  {
-    alert(" The book must have more than 30 pages ");
-    return false;
-  }
-}
-
-const title = 'My book';
-
-const text = `
-<div class="card">
-  <div class="card-body">
-    <h5 class="card-title">${title}</h5>
-    <h6 class="card-subtitle mb-2 text-muted">Author</h6>
-    <p class="card-text">Number of Pages</p>
-  </div>
-</div>`;
-
-const container = document.querySelector('.container');
-container.insertAdjacentHTML('beforeend', text);
